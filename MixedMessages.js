a9d46cda062c7489131c64b2313d54efdfad91e2
Tricia Gibson <88742030+TriciaGibson@users.noreply.github.com>
2021-12-13 18:40:20 -0500

    Update MixedMessages.js
    final commit

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,23 +1,24 @@
-const Message = () => {
+const Message1 = () => {
 let random = Math.floor(Math.random() * 3);
     switch (random) {
         case 0:
-            return 'Believe in yourself';
+            return 'You are enough';
         case 1:
             return 'You are smart';
         case 2: 
-        return 'You are loved';
-    }
-};
-console.log(Message());
-
-const Messages2 = () => {
-const arr2 = ['You are courageous', 'you are inspiring', 'you are important'];
-return arr2[Math.floor(Math.random() * 3)];
-};
-console.log(Messages2());
-
-const MessagesArray = ["You are strong", "Never give up", "Be kind"];
-for (let i = 0; i < MessagesArray.length; i++){
-    console.log(MessagesArray[i]);
-};
+            return 'You are loved';
+        }
+    };
+    console.log(Message1());
+    
+    const Message2 = () => {
+    const arr2 = ['You are courageous', 'You are inspiring', 'You are important'];
+    return arr2[Math.floor(Math.random() * 3)];
+    };
+    console.log(Message2());
+    
+    let Messages3 = () => {
+    let randomIndex = ['You are unique', 'You deserve happiness', 'You are capable'];
+    return randomIndex[Math.floor(Math.random() * 3)];
+    };
+    console.log(Messages3());
